44e56cfccca9a830fddbc717ec8e679fbd6b86f6
luckysaputra17 <damnlux17@gmail.com>
2022-12-02 01:20:18 +0700

    two commit

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,8 @@
-const burger = document.querySelector('.burger')
-const sidebar = document.querySelector('.sidebar')
-const bgSidebar = document.querySelector('.bg-sidebar')
-
-burger.addEventListener('click', function() {
-  this.classList.toggle('change')
-  sidebar.classList.toggle('change')
-  bgSidebar.classList.toggle('change')
-})
-
-bgSidebar.addEventListener('click', function() {
-  this.classList.remove('change')
-  sidebar.classList.remove('change')
-  burger.classList.remove('change')
-})
+function myFunction() {
+  var x = document.getElementById("checkbox_toggle");
+  if (x  == checked) {
+    
+  } else {
+    x.className = "container-flex";
+  }
+}
