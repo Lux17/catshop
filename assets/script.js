0e826e5b134c61ad48a5afa5aaa4d4f970ab1cb2
luckysaputra17 <damnlux17@gmail.com>
2022-12-02 12:01:57 +0700

    four commit

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,24 @@
-function myFunction() {
-  var x = document.getElementById("checkbox_toggle");
-  if (x  == checked) {
-    
-  } else {
-    x.className = "container-flex";
-  }
+function navSlide() {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav__links");
+  const navLinks = document.querySelectorAll(".nav__links li");
+  
+  burger.addEventListener("click", () => {
+      //Toggle Nav
+      nav.classList.toggle("nav-active");
+      
+      //Animate Links
+      navLinks.forEach((link, index) => {
+          if (link.style.animation) {
+              link.style.animation = ""
+          } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+          }
+      });
+      //Burger Animation
+      burger.classList.toggle("toggle");
+  });
+  
 }
+
+navSlide();
